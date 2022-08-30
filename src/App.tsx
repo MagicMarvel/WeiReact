d3c88e76b7cf0256ca4db28e6803b7632e9b92f3
Wei-1107 <yiweih893@gmail.com>
2022-08-30 22:28:27 +0800

    完成了函数式组件的适配

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { WeiReact } from "./Didact"; './Didact'
-const element = (
+import { WeiReact } from "./Didact";
+
+function Wei(props: any) {
+  return (
     <div>
-        <h1 className="hahaha">凯哥好厉害！</h1>
-        <h1 className="iiiiii">*\^_^/*</h1>
+      <h1 className="hahaha">凯哥好厉害！</h1>
+      <h1 className="iiiiii">*\^_^/*sssss</h1>
+      <Kai />
     </div>
-);
+  );
+}
 
+// function Kai(props: any) {
+//   return (
+//     <div>
+//       <h1>薇薇超可爱的！</h1>
+//     </div>
+//   );
+// }
+function Kai(props: any) {
+  return WeiReact.createElement(
+    "div",
+    null,
+    WeiReact.createElement("h1", null, "薇薇超可爱的！")
+  );
+}
 
-WeiReact.render(element, document.getElementById("root"));
+WeiReact.render(<Wei />, document.getElementById("root"));
--- src/Didact.tsx
+++ src/Didact.tsx
@@ -1,46 +1,67 @@
 type ElementInformation =
-    | {
-          //标签名
-          type: keyof HTMLElementTagNameMap;
-          props: {
-              //属性
-              [key: string]: any;
-              //子元素
-              children: ElementInformation[];
-          };
-      }
-    | {
-          type: "TEXT_ELEMENT";
-          props: {
-              nodeValue: string;
-          };
+  | {
+      //标签名
+      type: keyof HTMLElementTagNameMap;
+      props: {
+        //属性
+        [key: string]: any;
+        //子元素
+        children: ElementInformation[];
       };
+    }
+  | {
+      type: "TEXT_ELEMENT";
+      props: {
+        nodeValue: string;
+      };
+    }
+  | {
+      type: (props: any) => ElementInformation;
+      props: Object;
+    };
 
 type Fiber = {
-    dom: HTMLElement | Text;
-    child: Fiber;
-    silbing: Fiber;
-    parent: Fiber;
+  dom: HTMLElement | Text;
+  child: Fiber;
+  silbing: Fiber;
+  parent: Fiber;
 } & ElementInformation;
 
-function createElement(type: keyof HTMLElementTagNameMap, props: Object, ...children: (ElementInformation | string)[]): ElementInformation {
+/**
+ * 创建ElementInformation树
+ * @param type 普通标签或者函数
+ * @param props 普通标签的属性/函数组件的参数
+ * @param children 子元素
+ * @returns ElementInformation
+ */
+function createElement(
+  type: keyof HTMLElementTagNameMap | ((props: any) => ElementInformation),
+  props: Object,
+  ...children: (ElementInformation | string)[]
+): ElementInformation {
+  if (typeof type == "function")
     return {
-        type,
-        props: {
-            ...props,
-            children: children
-                ? children.map((child) => {
-                      if (typeof child === "string") {
-                          return {
-                              type: "TEXT_ELEMENT",
-                              props: { nodeValue: child },
-                          };
-                      }
-                      return child;
-                  })
-                : [],
-        },
+      type,
+      props,
     };
+
+  return {
+    type,
+    props: {
+      ...props,
+      children: children
+        ? children.map((child) => {
+            if (typeof child === "string") {
+              return {
+                type: "TEXT_ELEMENT",
+                props: { nodeValue: child },
+              };
+            }
+            return child;
+          })
+        : [],
+    },
+  };
 }
 
 /**
@@ -49,134 +70,188 @@ function createElement(type: keyof HTMLElementTagNameMap, props: Object, ...chil
  * @param container 要渲染到的容器
  */
 function render(element: ElementInformation, container: HTMLElement) {
-    let workInProgressRoot = {
-        type: element.type,
-        props: element.props,
-        dom: null,
-        child: null,
-        silbing: null,
-        parent: null,
-    } as Fiber;
-    let workInProgressFiber = workInProgressRoot;
+  let workInProgressRoot: Fiber = {
+    type: element.type,
+    props: element.props,
+    dom: null,
+    child: null,
+    silbing: null,
+    parent: {
+      child: null,
+      dom: container,
+      parent: null,
+      props: null,
+      silbing: null,
+      type: "div",
+    },
+  } as Fiber;
+  workInProgressRoot.parent.child = workInProgressRoot;
+  let nextUnitOfWork = workInProgressRoot;
+
+  /**
+   * 普通标签：
+   * 1.创建DOM元素
+   * 2.生成子元素的fiber
+   * 3.查找下一个fiber
+   *
+   * 函数式组件：
+   * 1.运行它，得到ElementInformation
+   * 2.根据ElementInformation生成一个fiber
+   * 3.查找下一个fiber
+   * @param deadline 剩余时间
+   */
+  function wookLoop(deadline: IdleDeadline): void {
     /**
-     * 1.将DOM树转化为二叉树
-     * 2.创建DOM元素
-     * @param deadline 剩余时间
+     * 创建DOM
+     * @param element 要渲染上去的元素的信息
+     * @returns dom元素
      */
-    function wookLoop(deadline: IdleDeadline): void {
-        /**
-         * 创建DOM
-         * @param element 要渲染上去的元素的信息
-         * @returns dom元素
-         */
-        function createDom(element: ElementInformation): HTMLElement | Text {
-            let dom: HTMLElement | Text;
-            if (element.type == "TEXT_ELEMENT") {
-                dom = document.createTextNode(element.props.nodeValue);
+    function createDom(element: ElementInformation): HTMLElement | Text {
+      // 函数式节点没有dom
+      if (typeof element.type == "function") {
+        throw new Error("传入function节点到createDom函数里面");
+      }
+      let dom: HTMLElement | Text;
+      if (element.type == "TEXT_ELEMENT") {
+        dom = document.createTextNode(element.props.nodeValue);
+      } else {
+        dom = document.createElement(element.type);
+        // .keys 是把属性提取出来
+        Object.keys(element.props)
+          .filter((key) => key != "children")
+          .forEach((key) => {
+            const value = element.props[key];
+            if (key.startsWith("on")) {
+              (dom as HTMLElement).addEventListener(
+                key.substring(2).toLowerCase(),
+                value
+              );
             } else {
-                dom = document.createElement(element.type);
-                // .keys 是把属性提取出来
-                Object.keys(element.props)
-                    .filter((key) => key != "children")
-                    .forEach((key) => {
-                        const value = element.props[key];
-                        if (key.startsWith("on")) {
-                            (dom as HTMLElement).addEventListener(key.substring(2).toLowerCase(), value);
-                        } else {
-                            (dom as HTMLElement).setAttribute(key, value);
-                        }
-                    });
+              (dom as HTMLElement).setAttribute(key, value);
             }
-            return dom;
+          });
+      }
+      return dom;
+    }
+
+    /**
+     * 创建子元素的fiber
+     * @param fiber 要被创建fiber的元素的父元素fiber
+     */
+    function createChildrenFiber(fiber: Fiber) {
+      if (typeof fiber.type == "function") {
+        throw new Error("传入function函数到createChildrenFiber了");
+      }
+      if (fiber.type == "TEXT_ELEMENT") {
+        return;
+      }
+
+      let isFirstChild = true;
+      let lastChild = null;
+      (fiber.props as { children: ElementInformation[] }).children.forEach(
+        (child) => {
+          const newFiber: Fiber = {
+            type: child.type,
+            props: child.props,
+            dom: null,
+            child: null,
+            silbing: null,
+            parent: null,
+          } as Fiber;
+          if (isFirstChild) {
+            fiber.child = newFiber;
+            newFiber.parent = fiber;
+            lastChild = newFiber;
+          } else {
+            lastChild.silbing = newFiber;
+            lastChild = newFiber;
+            newFiber.parent = fiber;
+          }
+          isFirstChild = false;
+        }
+      );
+    }
+
+    /**
+     * 找到下一个要渲染的fiber
+     * @param nextUnitOfWork 当前正在渲染的fiber
+     */
+    function findNextWorkInProgressFiber(nextUnitOfWork: Fiber): Fiber {
+      // throw new Error("Function not implemented.");
+      //如果有儿子，先访问左儿子
+      if (nextUnitOfWork.child) {
+        return nextUnitOfWork.child;
+      } else if (nextUnitOfWork.silbing) {
+        return nextUnitOfWork.silbing;
+      }
+      //如果没有儿子，就一直往上找有右儿子的爸爸
+      else {
+        let now = nextUnitOfWork;
+        while (now.parent) {
+          if (now.parent.silbing) return now.parent.silbing;
+          else now = now.parent;
+        }
+        return null;
+      }
+    }
+
+    function commitRoot(root: Fiber) {
+      function commitWork(fiber: Fiber) {
+        if (fiber == null) return;
+        if (!fiber.dom) {
+          commitWork(findNextWorkInProgressFiber(fiber));
+        } else {
+          let now = fiber;
+          while (!now.parent.dom) {
+            now = now.parent;
+            break;
+          }
+          now.parent.dom.appendChild(fiber.dom);
+          commitWork(findNextWorkInProgressFiber(fiber));
+        }
+      }
+      commitWork(root);
+    }
+
+    if (deadline.timeRemaining() > 30 && nextUnitOfWork != null) {
+      // 处理类似div、"Hello world!"这种节点
+      const dealingHostComponent = () => {
+        nextUnitOfWork.dom = createDom(nextUnitOfWork);
+        createChildrenFiber(nextUnitOfWork);
+      };
+
+      // 处理类似App这种节点
+      const dealingFunctionComponent = () => {
+        if (typeof nextUnitOfWork.type != "function") {
+          throw new Error(
+            "进入dealingFunctionComponent的时候,这个节点不是一个function节点"
+          );
         }
 
-        /**
-         * 将DOM树节点转化为二叉树节点
-         * @param fiber 要渲染的DOM元素的信息，要转换为二叉树的节点
-         */
-        function createChildrenFiber(fiber: Fiber) {
-            if (fiber.type != "TEXT_ELEMENT") {
-                let isFirstChild = true;
-                let lastChild = null;
-                fiber.props.children.forEach((child) => {
-                    const newFiber: Fiber = {
-                        type: child.type,
-                        props: child.props,
-                        dom: null,
-                        child: null,
-                        silbing: null,
-                        parent: null,
-                    } as Fiber;
-                    if (isFirstChild) {
-                        fiber.child = newFiber;
-                        newFiber.parent = fiber;
-                        lastChild = newFiber;
-                    } else {
-                        lastChild.silbing = newFiber;
-                        lastChild = newFiber;
-                        newFiber.parent = fiber;
-                    }
-                    isFirstChild = false;
-                });
-            }
-        }
+        const elementInformation = nextUnitOfWork.type(nextUnitOfWork.props);
 
-        /**
-         * 找到下一个要渲染的fiber
-         * @param workInProgressFiber 当前正在渲染的fiber
-         */
-        function findNextWorkInProgressFiber(workInProgressFiber: Fiber): Fiber {
-            // throw new Error("Function not implemented.");
-            //如果有儿子，先访问左儿子
-            if (workInProgressFiber.child) {
-                return workInProgressFiber.child;
-            }
-            else if(workInProgressFiber.silbing){
-                return workInProgressFiber.silbing;
-            }
-            //如果没有儿子，就一直往上找有右儿子的爸爸
-            else {
-                let now = workInProgressFiber;
-                while (now.parent) {
-                    if (now.parent.silbing) return now.parent.silbing;
-                    else now = now.parent;
-                }
-                return null;
-            }
-        }
+        const newFiber: Fiber = {
+          type: elementInformation.type,
+          props: elementInformation.props,
+          dom: null,
+          child: null,
+          parent: nextUnitOfWork,
+          silbing: null,
+        } as Fiber;
+        nextUnitOfWork.child = newFiber;
+      };
 
-        function commitRoot(root: Fiber) {
-            function commitWork(fiber: Fiber) {
-                if (fiber.type == "TEXT_ELEMENT") {
-                    return;
-                }
-                if (fiber.child) {
-                    fiber.dom.appendChild(fiber.child.dom);
-                    commitWork(fiber.child);
-                    let silbing = fiber.child.silbing;
-                    while (silbing) {
-                        fiber.dom.appendChild(silbing.dom);
-                        commitWork(silbing);
-                        silbing = silbing.silbing;
-                    }
-                }
-            }
-            commitWork(root);
-            container.appendChild(root.dom);
-        }
+      if (typeof nextUnitOfWork.type != "function") dealingHostComponent();
+      else dealingFunctionComponent();
+      nextUnitOfWork = findNextWorkInProgressFiber(nextUnitOfWork);
 
-
-        if (deadline.timeRemaining() > 30 && workInProgressFiber != null) {
-            workInProgressFiber.dom = createDom(workInProgressFiber);
-            createChildrenFiber(workInProgressFiber);
-            workInProgressFiber = findNextWorkInProgressFiber(workInProgressFiber);
-            if (workInProgressFiber == null) {
-                commitRoot(workInProgressRoot);
-            }
-        }
-        requestIdleCallback(wookLoop);
+      if (nextUnitOfWork == null) {
+        commitRoot(workInProgressRoot);
+      }
     }
     requestIdleCallback(wookLoop);
+  }
+  requestIdleCallback(wookLoop);
 }
 
 export const WeiReact = { createElement, render };
